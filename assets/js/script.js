53522e7e281976026a302ebc08360fa7c530bb32
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-12-11 18:11:23 +0000

    adds code to display time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 var timeEl = $('#currentDay');
 var rowsContainer = $('.container');
 // Display the current day at the top of the calendar
-var currentTime = moment().format("dddd, Do MMM YYYY");
+var currentTime = moment().format("dddd, MMMM Do");
 timeEl.text(currentTime);
 
 var workStartTime = moment(09, 'hours');
-// While loop that loops through business day hours
+// While loop that loops through business day hours and generate or build html timeblock row
 while (workStartTime.hour() < 18) {
-    rowsContainer.append(`<div class='time-block row'><p class="hour">${workStartTime.hour()}<p><textarea></textarea><button class='save.Btn'></button></div>`);
+    // Append timeblock to container
+        // Hour
+        // Textarea
+        // Save button
+    rowsContainer.append(`<div class='row time-block'><p class="hour col-md-1">${workStartTime.format('hh a')}</p><textarea class="col-md-10 description"></textarea><button class='btn saveBtn col-md-1'><i class="fas fa-archive"></i></button></div>`);
+    // Increase hour by one
     workStartTime.add(1, 'hours');
 }
-        // For each loop generate or build html timeblock row
+             // When button is clicked - store/reset the event text corresponding withthe hour to localStorage
+    // Check if hour is past, current or future and apply the corresponding CSS class to the timeblock
 
-            // Append timeblock to container
-                // Hour
-                // Textarea
-                // Save button
-                    // When button is clicked - store/reset the event text corresponding withthe hour to localStorage
-            // Increase hour by one
-            // Check if hour is past, current or future and apply the corresponding CSS class to the timeblock
