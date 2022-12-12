18a65ea384a8f841953faeebefd586170116a7d4
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-12-12 20:28:24 +0000

    adds cheages to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var timeEl = $('#currentDay');
 var rowsContainer = $('.container');
 // Display the current day at the top of the calendar
-var displayTime = moment().format("dddd, MMMM Do");
+var displayTime = moment().format('dddd, MMMM Do');
 timeEl.text(displayTime);
 
 var businessHours = moment().hour(9, 'H');
@@ -10,9 +10,9 @@ while (businessHours.hour() < 18) {
     var workHour = businessHours.format('H');
     var hourId = workHour;
     // Append timeblock to container
-        // Hour
-        // Textarea
-        // Save button
+    // Hour
+    // Textarea
+    // Save button
     rowsContainer.append(`<div class='row time-block'><p class="hour col-md-1">${businessHours.format('HA')}</p><textarea class="description col-md-10" id='${hourId}'></textarea><button class='btn saveBtn col-md-1'><i class="fas fa-archive"></i></button></div>`);
     // Increase hour by one
     businessHours.add(1, 'hours');
@@ -25,20 +25,20 @@ function checkTime() {
     var hourIdArray = [];
     // Loop through the textareas
     $('textarea').each(function () {
-        // Getting the id of each textarea
+        // Get the id of each textarea
         var textareaId = $(this).attr('id');
         // Add to the array
         hourIdArray.push(textareaId);
     });
     //Compare index value to currentTime
     for (i = 0; i < hourIdArray.length; i++) {
-        var textArea = $(`#${hourIdArray[i]}`);
-        if (hourIdArray[i] === currentTime) {
-            textArea.addClass("present");
-        } else if (hourIdArray[i] < currentTime) {
-            textArea.addClass("past");
+        var textarea = $(`#${hourIdArray[i]}`);
+        if (hourIdArray[i] < currentTime) {
+            textarea.addClass('past');
+        } else if (hourIdArray[i] === currentTime) {
+            textarea.addClass('present');
         } else {
-            textArea.addClass("future");
+            textarea.addClass('future');
         }
     }
 }
@@ -46,9 +46,9 @@ function checkTime() {
 // Grab button
 saveBtn = $('.saveBtn');
 // Add event listener
-saveBtn.on("click", function() {
-    var time = $(this).siblings(".hour").text();
-    var task = $(this).siblings(".description").val();
+saveBtn.on('click', function () {
+    var time = $(this).siblings('.hour').text();
+    var task = $(this).siblings('.description').val();
     // Save the text for that event to localStorage
     localStorage.setItem(time, task);
     // Confirm to user that data was saved by displaying a feedback
@@ -61,12 +61,12 @@ saveBtn.on("click", function() {
 
 // Create function to display the data that was previously saved 
 function getSavedTasks() {
-    $(".hour").each(function() {
+    $('.hour').each(function () {
         var savedHour = $(this).text();
         var savedTask = localStorage.getItem(savedHour);
 
-        if(savedTask !== null) {
-            $(this).siblings(".description").val(savedTask);
+        if (savedTask !== null) {
+            $(this).siblings('.description').val(savedTask);
         }
     });
 }
